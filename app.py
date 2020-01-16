ed61225d493f655e0fde1707aff7e6d167046f3c
benja8151 <benjamin.smrdelj@gmail.com>
2020-01-16 19:42:14 +0100

    added pairplot

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sb
+import random
 
-from negative_core import get_model_params, model, simulate
-# from positive_core import get_model_params, model, simulate
+#from negative_core import get_model_params, model, simulate
+from positive_core import get_model_params, model, simulate
 from utils import eval_signal, oscilating, find_peaks
 
 
@@ -73,20 +74,56 @@ def sampling(m_number, K_number, samples_number, graph_index):
     ax3.set_ylabel(var_list[3 + m_number])
 
 
+def pairplot(samples_number, m_range, K_range, base_amplituda, base_perioda):
+    ms = np.linspace(m_range[0], m_range[1], m_range[1]- m_range[0] + 1, dtype=int)
+    Ks = np.linspace(10**K_range[0], 10**K_range[1],samples_number)
+    oscilating_samples = []
+    print (base_amplituda, base_perioda)
+    while len(oscilating_samples)<10:
+        new_params = list(params)
+        new_params[4] = random.choice(ms)
+        new_params[5] = random.choice(ms)
+        new_params[6] = random.choice(ms)
+        new_params[7] = random.choice(Ks)
+        new_params[8] = random.choice(Ks)
+        new_params[9] = random.choice(Ks)
+
+        A, B, C = simulate(model, tuple(new_params))
+        peaks_A, minimums_A, peaks_vals_A, minimums_vals_A = find_peaks(A)
+
+        if oscilating(peaks_vals_A, 0.01):
+            amplituda, perioda = eval_signal(peaks_A, minimums_vals_A, peaks_vals_A)
+            if (amplituda > 1  and perioda<base_perioda):#(amplituda >= base_amplituda and perioda<=base_perioda):
+                oscilating_samples.append(list(new_params[4:]))
+                print(len(oscilating_samples), amplituda, perioda)
+    
+    df = pd.DataFrame(np.array(oscilating_samples), columns=["m1", "m2", "m3", "K1", "K2", "K3"])
+    sb.pairplot(df, markers="+")
+
+
+
 params = get_model_params()
+
+# base model vrednosti
+A, B, C = simulate(model, params)
+peaks_A, minimums_A, peaks_vals_A, minimums_vals_A = find_peaks(A)
+base_amplituda, base_perioda = eval_signal(peaks_A, minimums_vals_A, peaks_vals_A)
 
 # fig = plt.figure(figsize=(100, 10))
 # fig2 = plt.figure(figsize=(100, 10))
 # fig3 = plt.figure(figsize=(100, 10))
 
-fig1 = plt.figure(figsize=(25, 10))
-fig2 = plt.figure(figsize=(25, 10))
-fig3 = plt.figure(figsize=(25, 10))
+#fig1 = plt.figure(figsize=(25, 10))
+#fig2 = plt.figure(figsize=(25, 10))
+#fig3 = plt.figure(figsize=(25, 10))
 
-preveri_obmocje(50)
+#preveri_obmocje(50)
+pairplot(10000, [1,4], [-1,3], base_amplituda, base_perioda)
 # ce zelis spremeniti tip modela (poz. v neg. povratno zanko samo spremeni importe)
 
 # SHRANJEVANJE GRAFOV
 # plt.savefig('testplot.eps', format='eps')
+
+
 
 plt.show()
