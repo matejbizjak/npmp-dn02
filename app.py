128c8dc3019a5a81d712b5e89846e284f1e09256
matej <matejb96@gmail.com>
2020-01-19 17:23:11 +0100

    m1=m2=m3=K1=K2=K3 heatmaps

--- app.py
+++ app.py
@@ -12,63 +12,53 @@ from pyDOE import lhs  # pip install pyDOE
 from utils import eval_signal, oscilating, find_peaks
 
 
-# def preveri_obmocje(samples_number):
-#     # vse kombinacije m-jev in K-jev
-#     graph_index = 1
-#     for i in range(1, 4):
-#         for j in range(1, 4):
-#             print(graph_index, 'out of ', 9)
-#             sampling(i, j, samples_number, graph_index)
-#             graph_index += 1
-#
-#
-# def sampling(m_number, K_number, samples_number, graph_index):
-#
-#     # params = (alpha, alpha0, beta, n, m1, m2, m3, K1, K2, K3)
-#     new_params = list(params)
-#
-#     # zgradimo prazne matrike, kamor bomo shranjevali rezultate
-#     oscilacije = [[np.nan for x in range(samples_number)] for x in range(4)]
-#     amplitude = [[np.nan for x in range(samples_number)] for x in range(4)]
-#     periode = [[np.nan for x in range(samples_number)] for x in range(4)]
-#
-#     # sampling
-#     m_samples = [4, 3, 2, 1]
-#     # K_samples = np.logspace(-1, 3, samples_number)
-#     K_samples = np.linspace(0.1, 1000, samples_number)
-#
-#     # za vse sample pozenemo simulacijo in rezultate shranimo v prej narejene matrike
-#     for x in range(len(m_samples)):
-#         print('\t', x + 1, 'out of ', len(m_samples))
-#         for y in range(len(K_samples)):
-#             new_params[3 + m_number] = m_samples[x]
-#             new_params[6 + K_number] = K_samples[y]
-#
-#             A, B, C = simulate(model, tuple(new_params))
-#
-#             peaks_A, minimums_A, peaks_vals_A, minimums_vals_A = find_peaks(A)
-#
-#             if oscilating(peaks_vals_A, 0.01):
-#                 oscilacije[x][y] = 1
-#
-#                 amplituda, perioda = eval_signal(peaks_A, minimums_vals_A, peaks_vals_A)
-#                 amplitude[x][y] = amplituda
-#                 periode[x][y] = perioda
-#
-#     # narisemo grafe
-#     plot_graph(oscilacije, m_number, K_number, m_samples, K_samples, graph_index, fig_osc, 'Oscilacije')
-#     plot_graph(amplitude, m_number, K_number, m_samples, K_samples, graph_index, fig_amp, 'Amplitude')
-#     plot_graph(periode, m_number, K_number, m_samples, K_samples, graph_index, fig_per, 'Periode')
+def preveri_obmocje(samples_number):
+    # params = (alpha, alpha0, beta, n, m1, m2, m3, K1, K2, K3)
+    new_params = list(params)
+
+    # zgradimo prazne matrike, kamor bomo shranjevali rezultate
+    oscilacije = [[np.nan for x in range(samples_number)] for x in range(4)]
+    amplitude = [[np.nan for x in range(samples_number)] for x in range(4)]
+    periode = [[np.nan for x in range(samples_number)] for x in range(4)]
+
+    # sampling
+    m_samples = [4, 3, 2, 1]
+    K_samples = np.linspace(0.1, 1000, samples_number)
+
+    # za vse sample pozenemo simulacijo in rezultate shranimo v prej narejene matrike
+    for x in range(len(m_samples)):
+        for y in range(len(K_samples)):
+            new_params[4] = m_samples[x]
+            new_params[5] = m_samples[x]
+            new_params[6] = m_samples[x]
+            new_params[7] = K_samples[y]
+            new_params[8] = K_samples[y]
+            new_params[9] = K_samples[y]
+
+            A, B, C = simulate(model, tuple(new_params))
+
+            peaks_A, minimums_A, peaks_vals_A, minimums_vals_A = find_peaks(A)
+
+            if oscilating(peaks_vals_A, 0.01):
+                oscilacije[x][y] = 1
+
+                amplituda, perioda = eval_signal(peaks_A, minimums_vals_A, peaks_vals_A)
+                amplitude[x][y] = amplituda
+                periode[x][y] = perioda
+
+    # narisemo grafe
+    plot_graph(oscilacije, m_samples, K_samples, fig_osc, 'Oscilacije')
+    plot_graph(amplitude, m_samples, K_samples, fig_amp, 'Amplitude')
+    plot_graph(periode, m_samples, K_samples, fig_per, 'Periode')
 
 
-# def plot_graph(data, m_number, K_number, m_samples, K_samples, graph_index, figure, figure_title):
-#     data_frame = pd.DataFrame(data, columns=list(map(lambda k: round(k, 3), K_samples)), index=m_samples)
-#     subplot = figure.add_subplot(3, 3, graph_index)
-#     figure.suptitle(figure_title, fontsize=30)
-#     sb.heatmap(data_frame, ax=subplot, cbar=True, square=False, mask=data_frame.isnull(), cmap="YlGnBu")
-#     var_list = ["alpha", "alpha0", "beta", "n", "m1", "m2", "m3", "K1", "K2", "K3"]
-#     subplot.set_xlabel(var_list[6 + K_number])
-#     subplot.set_ylabel(var_list[3 + m_number])
+def plot_graph(data, m_samples, K_samples, figure, figure_title):
+    data_frame = pd.DataFrame(data, columns=list(map(lambda k: int(k), K_samples)), index=m_samples)
+    subplot = figure.add_subplot(1, 1, 1)
+    figure.suptitle(figure_title, fontsize=30)
+    sb.heatmap(data_frame, ax=subplot, cbar=True, square=False, mask=data_frame.isnull(), cmap="YlGnBu")
+    subplot.set_xlabel("K1 = K2 = K3")
+    subplot.set_ylabel("m1 = m2 = m3")
 
 
 def pairplot(samples_number, m_range, K_range, base_amplituda, base_perioda):
@@ -109,7 +99,7 @@ def denormalize(normalized_value, min, max, discrete=False):
             if normalized_value < start_splitter:
                 return values[counter]
             counter += 1
-            start_splitter += splitter;
+            start_splitter += splitter
         return values[-1]
     else:
         value = ((max - min) * normalized_value) + min
@@ -174,10 +164,12 @@ model_type = 'positive'
 
 # figure_size_x = 100
 # figure_size_y = 15
-# figure_size_x = 30
-# figure_size_y = 15
+figure_size_x = 15
+figure_size_y = 15
 
-# samples_number = 100
+sb.set(font_scale=2)
+
+samples_number = 250
 # END OF USER PARAMETERS
 
 # SETUP
@@ -190,12 +182,12 @@ simulate = _temp.simulate
 
 params = get_model_params()
 
-# fig_osc = plt.figure(figsize=(figure_size_x, figure_size_y))
-# fig_amp = plt.figure(figsize=(figure_size_x, figure_size_y))
-# fig_per = plt.figure(figsize=(figure_size_x, figure_size_y))
+fig_osc = plt.figure(figsize=(figure_size_x, figure_size_y))
+fig_amp = plt.figure(figsize=(figure_size_x, figure_size_y))
+fig_per = plt.figure(figsize=(figure_size_x, figure_size_y))
 # END OF SETUP
 
-# preveri_obmocje(samples_number)
+preveri_obmocje(samples_number)
 
 # base model vrednosti
 A, B, C = simulate(model, params)
@@ -203,25 +195,25 @@ peaks_A, minimums_A, peaks_vals_A, minimums_vals_A = find_peaks(A)
 base_amplituda, base_perioda = eval_signal(peaks_A, minimums_vals_A, peaks_vals_A)
 
 # pairplot(10000, [1, 4], [-1, 3], base_amplituda, base_perioda)
-pairplot_lhs(200000, [1, 4], [0.1, 1000], base_amplituda, base_perioda)
+# pairplot_lhs(200000, [1, 4], [0.1, 1000], base_amplituda, base_perioda)
 
 # SAVE GRAPHS
-# print('Saving graphs...')
+print('Saving graphs...')
 current_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
 # eps
-# fig_osc.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_osc' + str(samples_number) + '.eps',
-#                format='eps')
-# fig_amp.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_amp' + str(samples_number) + '.eps',
-#                format='eps')
-# fig_per.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_per' + str(samples_number) + '.eps',
-#                format='eps')
+fig_osc.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_osc' + str(samples_number) + '.eps',
+                format='eps')
+fig_amp.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_amp' + str(samples_number) + '.eps',
+                format='eps')
+fig_per.savefig('plots/temp/eps/' + model_type[0] + '_' + str(current_time) + '_per' + str(samples_number) + '.eps',
+                format='eps')
 # png
-# fig_osc.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_osc' + str(samples_number) + '.png',
-#                format='png')
-# fig_amp.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_amp' + str(samples_number) + '.png',
-#                format='png')
-# fig_per.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_per' + str(samples_number) + '.png',
-#                format='png')
+fig_osc.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_osc' + str(samples_number) + '.png',
+                format='png')
+fig_amp.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_amp' + str(samples_number) + '.png',
+                format='png')
+fig_per.savefig('plots/temp/png/' + model_type[0] + '_' + str(current_time) + '_per' + str(samples_number) + '.png',
+                format='png')
 
 # plt.show()
 # END OF SAVE GRAPHS
